48f2fb07b8b62ca6d7c8cde974ab2a8c3ffb32f9
cparlette <cparlette@gmail.com>
2017-11-07 22:43:28 -0500

    Hookup to API Gateway

--- foodfight/src/config.js
+++ foodfight/src/config.js
@@ -1,7 +1,14 @@
 export default {
   MAX_ATTACHMENT_SIZE: 5000000,
+  apiGateway: {
+    URL: "https://vopwrk2tik.execute-api.us-east-1.amazonaws.com/prod/",
+    REGION: "us-east-1"
+  },
   cognito: {
     USER_POOL_ID: "us-east-1_62I0VfrtF",
     APP_CLIENT_ID: "636doki25mo484vct1lbrtvo81"
+    REGION: "us-east-1",
+    IDENTITY_POOL_ID: "us-east-1_62I0VfrtF"
   }
 };
+
