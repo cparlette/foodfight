2e66ee36f233865cba18c5b3f3cfc99199407d28
cparlette <cparlette@gmail.com>
2017-11-12 18:27:13 -0500

    Add list of notes to home screen, fix some config

--- foodfight/src/config.js
+++ foodfight/src/config.js
@@ -1,7 +1,7 @@
 export default {
   MAX_ATTACHMENT_SIZE: 5000000,
   s3: {
-    BUCKET: "notes-app-api-prod-serverlessdeploymentbucket-84l437ujrdcq"
+    BUCKET: "notes-cparlette-test"
   },
   apiGateway: {
     URL: "https://vopwrk2tik.execute-api.us-east-1.amazonaws.com/prod/",
@@ -9,9 +9,9 @@ export default {
   },
   cognito: {
     USER_POOL_ID: "us-east-1_62I0VfrtF",
-    APP_CLIENT_ID: "636doki25mo484vct1lbrtvo81"
+    APP_CLIENT_ID: "636doki25mo484vct1lbrtvo81",
     REGION: "us-east-1",
-    IDENTITY_POOL_ID: "us-east-1_62I0VfrtF"
+    IDENTITY_POOL_ID: "us-east-1:1b0f2e37-0c8b-42ce-a81c-26a3f358f13a"
   }
 };
 
