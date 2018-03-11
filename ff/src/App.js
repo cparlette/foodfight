05f5b121a291faf1e4b7dc1e2bf7f7f393f362c1
cparlette <cparlette@gmail.com>
2018-03-10 19:17:48 -0500

    Added form that inserts into DynamoDB

--- ff/src/App.js
+++ ff/src/App.js
@@ -42,6 +42,61 @@ class Square extends React.Component {
   }
 }
 
+class UserForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      fname: '',
+      lname: '',
+      email: '',
+    };
+  }
+
+  onChange = (e) => {
+    // Because we named the inputs to match their corresponding values in state, it's
+    // super easy to update the state
+    const state = this.state
+    state[e.target.name] = e.target.value;
+    this.setState(state);
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    // get our form data out of state
+    const { fname, lname, email } = this.state;
+
+    axios.put('https://5ughd7di2g.execute-api.us-east-1.amazonaws.com/dev/putNewItemInDynamo', {
+      fname: fname,
+      lname: lname,
+      email: email
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+  }
+
+  render() {
+    const { fname, lname, email } = this.state;
+    return (
+      <form onSubmit={this.onSubmit}>
+        <label> First Name:
+          <input type="text" name="fname" value={fname} onChange={this.onChange} />
+        </label><br />
+        <label> Last Name:
+          <input type="text" name="lname" value={lname} onChange={this.onChange} />
+        </label><br />
+        <label> Email Address:
+          <input type="text" name="email" value={email} onChange={this.onChange} />
+        </label><br />
+        <button type="submit">Submit</button>
+      </form>
+    );
+  }
+}
+
 class Board extends React.Component {
   renderSquare(i) {
     return <Square value={i} />;
@@ -53,6 +108,8 @@ class Board extends React.Component {
     return (
       <div>
         <div className="status">{status}</div>
+        <p />
+        <UserForm />
         <p />
         <div className="buttons">
           {this.renderSquare('Increse Value in DynamoDB')}
