d5d4cdbe54d2e75b13e87272843e03a540803924
cparlette <cparlette@gmail.com>
2018-03-13 21:36:26 -0400

    Remove text from header

--- ff/src/App.js
+++ ff/src/App.js
@@ -8,8 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Food Fight!</h1>
+          <img src={logo} className="App-logo" alt="Food Fight!" />
         </header>
         <div className="App-intro">
           <Board />
