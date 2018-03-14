34aef4b9c0eae11f9e14a40c35a53103a5f71849
cparlette <cparlette@gmail.com>
2018-03-13 21:34:08 -0400

    Change the logo and stop it from spinning

--- ff/src/App.js
+++ ff/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import logo from './logo.svg';
+import logo from './FoodFightLogo.png';
 import './App.css';
 
 class App extends Component {
