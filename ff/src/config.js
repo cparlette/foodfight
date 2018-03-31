b4721bb6d7487eb1471695a8b50219269d2d85b9
cparlette <cparlette@gmail.com>
2018-03-31 11:11:08 -0400

    Amplify and Cognito setup start

---
+++ ff/src/config.js
@@ -0,0 +1,8 @@
+export default {
+	cognito: {
+		REGION: "us-east-1",
+		USER_POOL_ID: "us-east-1_VJcOWM82e",
+		APP_CLIENT_ID: "1sh6ip90mkjc5vl7710am2sgnv",
+		IDENTITY_POOL_ID: "us-east-1:2df711dd-8d14-43c0-897a-dd186f6e228d"
+	}
+}
