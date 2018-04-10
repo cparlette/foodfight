0bc27cc3366487de5e94b0777cce49dbedf1594f
cparlette <cparlette@gmail.com>
2018-04-10 14:15:30 -0400

    Add login page

--- ff/src/Routes.js
+++ ff/src/Routes.js
@@ -3,10 +3,12 @@ import { Route, Switch } from "react-router-dom";
 import { Status } from "./App";
 import ListUsers from "./ListUsers";
 import UserForm from "./UserForm";
+import Login from "./Login";
 
 export default ({ childProps }) =>
   <Switch>
     <Route path="/" exact component={Status} props={childProps} />
     <Route path="/put" exact component={UserForm} props={childProps} />
     <Route path="/get" exact component={ListUsers} props={childProps} />
+    <Route path="/login" exact component={Login} props={childProps} />
   </Switch>;
